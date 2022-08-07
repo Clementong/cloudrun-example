849f3db1966f333e55a3d27dac7d513b8da68d77
cøsq <54763947+Clementong@users.noreply.github.com>
2022-08-07 23:33:30 +0800

    changes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+import os
 from flask import Flask, render_template
 
 DEVELOPMENT_ENV  = True
@@ -38,4 +38,4 @@ def contact():
 
 
 if __name__ == '__main__':
-    app.run(debug=DEVELOPMENT_ENV)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
